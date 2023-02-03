24053560f3695afa2d94dc0b91283fccb60b6fa7
zyfrost <112628177+zyfrost@users.noreply.github.com>
2023-02-03 19:51:41 +0530

    added a love calcu code
    and added a folder

---
+++ javascriptsmallpro./lovecalci.js
@@ -0,0 +1,15 @@
+ var f1 = prompt("Enter First Name ");
+var f2 = prompt("Enter Second Name");
+var n = Math.random();
+n =Math.floor(n*100)+1;
+if (n>70) {
+ alert("The love percentage of "+ f1 +" and "+f2+" is "+n+" % . You Guys are ye fans" )
+    
+}
+    if (n>30&&n<=70) {
+         alert("The love percentage of "+ f1 +" and "+f2+" is "+n+" % . You Guys are nigga fans" )
+        
+    }
+    if(n<=30){
+    alert("The love percentage of "+ f1 +" and "+f2+" is "+n+" % . You GUys are FUddu NO.1" );
+}
